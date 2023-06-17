ba39ff05cb1386f27bc3478dde4713617537e896
Ray Saracual <rsaracual@plataformagroup.cl>
2023-06-17 19:38:52 -0400

    Settings: Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://raysaracual.github.io',
+  base: '/lawyer-page',
 });
